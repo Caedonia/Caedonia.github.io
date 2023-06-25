75a7741256da0b367f63abcf132a17c0d02e2ed2
Caedonia <118523982+Caedonia@users.noreply.github.com>
2023-06-25 21:39:48 +0200

    Update sendMail.js

--- sendMail.js
+++ sendMail.js
@@ -1,15 +1,17 @@
-function sendMail {
+function sendMail() {
     const maiL = document.getElementById('emailadd');
     const wiadOmosc = document.getElementById('wiadomosc');
     const imieNaz = document.getElementById("fname")+ " " + document.getElementById("lname");
     const poZiom = document.getElementById("poziom");
     const teLL = document.getElementById("telnumm");
-    const myMail = "knabelaleksandra@gmail.com"
+    const wiadoMosc2 = "Imię i Nazwisko: "+imieNaz+"\n" +"E-mail: "+ mail+ "\n"+"Numer Telefonu: "+teLL+"\n"+"Poziom"+poZiom+"\n"+ wiadOmosc;
+    console.log(wiadoMosc2);
+    return wiadoMosc2
+}
 
-    const wiadoMosc2 = "Imię i Nazwisko: "+imieNaz _
-                        +"Numer Telefonu: "+teLL
-                        +poZiom
-                        + wiadOmosc
-                 
-                        
+function wySliJJ (){
+    const myMail = "knabelaleksandra@gmail.com"
+    const magFrom = sendMail()
+
+
 }
