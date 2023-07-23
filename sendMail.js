3bcfac44a783cf406f8fd06a7a034ebc1f2d7b49
Caedonia <118523982+Caedonia@users.noreply.github.com>
2023-07-23 18:45:07 +0200

    email sending\

--- sendMail.js
+++ sendMail.js
@@ -1,27 +1,13 @@
-src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"
-
-
-function sendMail() {
-    const maiL = document.getElementById('emailadd');
-    const wiadOmosc = document.getElementById('wiadomosc');
-    const imieNaz = document.getElementById("fname")+ " " + document.getElementById("lname");
-    const poZiom = document.getElementById("poziom");
-    const teLL = document.getElementById("telnumm");
-    const wiadoMosc2 = "Imię i Nazwisko: "+imieNaz+"\n" +"E-mail: "+ maiL+ "\n"+"Numer Telefonu: "+teLL+"\n"+"Poziom"+poZiom+"\n"+ wiadOmosc;
-    console.log(wiadoMosc2);
-    return wiadoMosc2
-}
+window.onload = function(){
+document.getElementById('wyslij').addEventListener('click', wySliJJ);
+console.log("im running");}
 
 function wySliJJ (){
-    document.getElementById('wyslij').addEventListener('click',function(){
-        emailjs.init("bbaBDm_e0rMMG4bZQ");
-
 emailjs.send("service_pqniaal","template_zazqo39",{
 from_name: document.getElementById("fname")+" "+document.getElementById("lname"),
 to_name: "Aleksandra Knabel",
 message: "numer telefonu: "+document.getElementById("telnumm")+" poziom: "+document.getElementById("poziom")+" "+document.getElementById("wiadomosc"),
 reply_to: document.getElementById("emailadd")
 });
-    })
-    
-}
+console.log("it run");
+    }
