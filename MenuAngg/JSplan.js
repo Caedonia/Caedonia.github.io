991af816df1bba0d0c28daf773de1cc7d015ef29
Caedonia <118523982+Caedonia@users.noreply.github.com>
2024-01-06 13:51:19 +0100

    plany base

---
+++ MenuAngg/JSplan.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    document.getElementById('pon').addEventListener('click');
+    console.log("im running");
+    document.getElementById('wt').addEventListener('click')}
+
+function getPlann (){
+    
+}
