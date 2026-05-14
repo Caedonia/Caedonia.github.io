d56e2468b0add2604dc2fb80188ede8de445feca
Caedonia <118523982+Caedonia@users.noreply.github.com>
2026-05-14 21:56:27 +0200

    config

---
+++ JS/config.js
@@ -0,0 +1,7 @@
+
+const CONFIG = {
+    SUPABASE_URL: 'https://vjbrfvxbwjtncmvxeswb.supabase.co',
+    SUPABASE_ANON_KEY: 'sb_publishable_RlsEaUV3JWpRKRBZmai-tg_oJUFAvOA' 
+};
+
+export default CONFIG;
